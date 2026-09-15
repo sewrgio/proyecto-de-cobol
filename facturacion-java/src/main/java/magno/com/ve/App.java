2e628a2171ba64117360a5893cc93f0ba559697a
sergio <aj7559410@gmail.com>
2026-09-14 20:49:34 -0400

    java facturacion

---
+++ facturacion-java/src/main/java/magno/com/ve/App.java
@@ -0,0 +1,13 @@
+package magno.com.ve;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
